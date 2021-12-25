e9c75993c5ea1e6e64c818194028e81fd606959c
amyglan <f2016197@pilani.bits-pilani.ac.in>
2021-12-25 21:49:50 +0530

    ropsten infura deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,12 +10,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const privateKey = 'd5a339f00f4fab36900cb27c942057c3995fe4ce6bf518bf5dfe9eddb5f10b7c'
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    ropsten: {
+        url: 'https://ropsten.infura.io/v3/77115852fa3a440fb6519a95e87dc64b',
+        accounts: [`0x${privateKey}`]
+    }
+  }
 };
